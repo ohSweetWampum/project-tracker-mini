beee2b78f8e30a9308e06a1405ac02df1c4ac349
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-04-05 13:34:11 -0500

    added JS

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
+//display current day and time at top of the page
+function displayCurrentDateAndTimeAtTop(){
+  $("#currentDay").text(dayjs().format("MMM D, YYYY at hh:mm:ss a"));
+}
+
+displayCurrentDateAndTimeAtTop();
 
 
 
+
+//function to open bootstrap modal
+
+
+
+
+//Save the user inputs to local storage
+
+$(function() {
+  //select the element with the class .btn.saveBtn.col-2.col-md-1"
+  var submitButton = $(".btn.submitBtn.col-2.col-md-1");
+  submitButton.click(function() {
+   
+    var taskText = $(this).siblings(".description").val();
+    var taskTime = $(this).parent().attr("id");
+
+    storeToLocal({
+      taskText: taskText,
+      taskTime: taskTime
+    });
+    
+  });
+});
 
 
 
